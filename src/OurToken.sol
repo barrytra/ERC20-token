5cb9d350effffaf204900402d7b8e233505ef5a7
barrytra <BHAVESHGUPTA1602@GMAIL.COM>
2023-08-22 17:21:13 +0530

    tests done

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -1,8 +1,11 @@
+// contracts/OurToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
-import{ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
-    constructor 
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
 }
